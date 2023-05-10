59dc97bf05db2fd4ac0077f90d23b7122e226a8d
Joalisson Barros <joalissonricardo@gmail.com>
2023-05-09 23:01:15 -0300

    first commit :)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
 # Changelog
 
-All notable changes to `:package_name` will be documented in this file.
+All notable changes to `laravel-dump-ordered` will be documented in this file.
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace Jrbarros\LaravelDumpOrdered\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
